93ee1f9642d04b5de3491fbcb166a30e9115e6bd
nabeel saleem <ssbasemaker@gmail.com>
2025-11-12 19:00:51 +0500

    Delete qr_code_generator directory

--- qr_code_generator/types.ts
+++
@@ -1,3 +0,0 @@
-export type DotType = "square" | "dots" | "rounded" | "extra-rounded" | "classy" | "classy-rounded";
-export type CornerType = "square" | "extra-rounded" | "dot";
-export type ErrorCorrectionLevel = 'L' | 'M' | 'Q' | 'H';
