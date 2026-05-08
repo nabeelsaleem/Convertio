8b5984b5074fe7d60c3eb6ae67a5a5f1ce03b011
Huzaif-Innova360 <muhammadhuzaif@innova360.io>
2026-05-08 17:07:47 +0500

    new css minify

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#da3f0b',
+        'primary-hover': '#b53409',
+        'primary-light': '#fff5f2',
+        'primary-border': '#ffd1c4',
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    }
+  }
+}
